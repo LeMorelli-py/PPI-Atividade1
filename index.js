1db2ef47d291f5734b05eaa2c2cfb841f5eaabc4
Leandro Morelli <leandromorelli.adv@gmail.com>
2024-02-19 11:35:48 -0300

    up

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const host='0.0.0.0'; //O ip 0.0.0.0 representa todas as interfaces (placas de r
 const porta = 3000;  //Porta identifica um programa em execução no host hospedeiro
 const app = express();
 
-app.use(express.urlencoded({extended: true})); //biblioteca qs
+//app.use(express.urlencoded({extended: true})); //biblioteca qs
 
 //gerencie uma sessão como sendo uma espécie de memória adquirida pelo servidor para lembrar com quem ele conversa
 app.use(session({
@@ -20,26 +20,21 @@ app.use(session({
    }
 }))
 
+//O express oferece funcionalidades para permitir que conteúdo estático seja fornecido
+
 app.use(express.static(path.join(process.cwd(), 'publico')));
 
 app.use(autenticar, express.static(path.join(process.cwd(), 'privado')));
 
 app.post('/login', (requisicao, resposta)=>{
-   const usuario = requisicao.body.usuario;
-    const senha   = requisicao.body.senha;
+    const { usuario, senha } = requisicao.body;
     if (usuario && senha && usuario === 'Leandro' && senha === '0526'){
-        requisicao.session.usuarioLogado = true
-        next()
-    }
-   else{
-  resposta.redirect('/login.html');
+        requisicao.session.usuarioLogado = true;
+        resposta.redirect('/main.html');
+    } else{
+        resposta.redirect('/login.html');
    }
 })
-
-
-
-
-//O express oferece funcionalidades para permitir que conteúdo estático seja fornecido
 
 
 app.listen(porta, host, ()=>{
