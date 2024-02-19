2382deb49dd77518e33b88189f96aa78ed8a8e40
Leandro Morelli <leandromorelli.adv@gmail.com>
2024-02-19 10:52:51 -0300

    update

--- seguranca/autenticar.js
+++ seguranca/autenticar.js
@@ -1,6 +1,6 @@
 export default function autenticar(requisicao, resposta, next){
     if (requisicao.session?.usuarioLogado){
-        next();
+        next()
     }
     else{
         resposta.redirect('/login.html');
